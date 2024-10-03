57a5dcbbd7f743fec057ebefec09c99ec11d108e
Antoine Déchappe <dechantoine@gmail.com>
2024-10-03 15:44:03 +0200

    feat: explicit exceptions

--- bpmn_python/bpmn_diagram_exception.py
+++ bpmn_python/bpmn_diagram_exception.py
@@ -13,3 +13,32 @@ class BpmnPythonError(Exception):
 
     def __str__(self):
         return repr(self.value)
+
+class BpmnNodeTypeError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+class BpmnNodeNotFoundError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+class BpmnFlowNotFoundError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+
+class BpmnConnectedFlowsError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
